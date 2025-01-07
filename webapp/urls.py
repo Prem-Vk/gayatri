137868a09140a01ec6dc1d8be949846eddbdac7c
Prem-Vk <prem.vishwakarma@instamojo.com>
2025-01-07 16:46:41 +0530

    Replace new corousel image

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from webapp.views import HomePage, ProductPage,CategoryWiseProduct, search, add_client
-from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path("", cache_page(60*60)(HomePage.as_view()), name="home_page"),
+    path("", HomePage.as_view(), name="home_page"),
     path("product/<int:id>/", ProductPage.as_view(), name="product"),
     path("category/<int:id>/", CategoryWiseProduct.as_view(), name="category"),
     path('search/', search, name='search_product'),
